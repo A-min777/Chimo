a8c2b066a4a4b88e8ae70b0f07e971f5c86ed94c
Amin Chen <minde85731@gmail.com>
2024-09-01 23:06:35 +0800

    新增產品頁

--- js/myheader.js
+++ js/myheader.js
@@ -1,8 +1,9 @@
-const myHeader = {template:`
+const myHeader = {
+  template: `
     <header v-if="showAd" class="ad-banner text-center ">Welcom to Chimo! 
       <button v-on:click="closeAd" class="btn btn-outline-light btn-sm ms-2 ps-">X</button>
     </header>
-    <header class="navbar d-flex ps-5 pe-5 border-bottom border-2">
+    <header class="navbar d-flex pt-3 border-bottom border-2">
        <a href="index.html"><img src="../images/LOGO.png" alt="Logo" height="60px" class="ps-5"></a>
        <button class="btn btn-lg ps-5">分類▼</button>
        <form id="searchbar" class=" d-flex" role="search">
@@ -16,11 +17,15 @@ const myHeader = {template:`
          </ul>
        </nav>
     </header>`,
-  setup() {      
+  setup() {
     const showAd = ref(true);
     const closeAd = () => {
       showAd.value = false;
-    }
-    return { showAd, closeAd };
+    };
+
+    return {
+      showAd,
+      closeAd,
+    };
   },
 };
